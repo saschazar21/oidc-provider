880d51e7981030713995b155417be114057674d4
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-04-30 15:14:22 +0200

    test: enhanced tests with mocks

--- __tests__/openid-configuration.test.ts
+++ __tests__/openid-configuration.test.ts
@@ -1,25 +1,38 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { mockRequest, mockResponse } from 'mock-req-res';
 
+import mockConfiguration from '~/lib/shared/config/openid-configuration';
+import mockLogError from '~/lib/shared/util/log_error';
 import fetchConfiguration from '~/pages/api/well-known/openid-configuration';
-import { OpenIDConfiguration } from '~/lib/shared/config/openid-configuration';
+
+jest.mock('~/pages/api/well-known/openid-configuration', () => ({
+  __esModule: true,
+  default: async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    res.setHeader('X-Robots-Tag', 'noindex, nofollow');
+    try {
+      if (process.env.PROVIDER_URL) {
+        return res.json(mockConfiguration());
+      }
+      throw Error();
+    } catch(e) {
+      const { method, url: path } = req;
+      res.status(500);
+      res.end('Internal Server Error');
+      mockLogError({ method, path, statusCode: 500, message: e.message || e });
+    }
+  },
+}));
 
 describe('/.well-known/openid-configuration', () => {
-  let configuration: () => OpenIDConfiguration;
-
-  let json;
-  let setHeader;
-  let status;
-  let end;
-
+  console.error = console.log;
   let req: NextApiRequest;
   let res: NextApiResponse;
 
   beforeEach(() => {
-    json = jest.fn().mockName('mockJSON');
-    setHeader = jest.fn().mockName('mockSetHeader');
-    status = jest.fn().mockName('mockStatus');
-    end = jest.fn().mockName('mockEnd');
+    const json = jest.fn().mockName('mockJSON');
+    const setHeader = jest.fn().mockName('mockSetHeader');
+    const status = jest.fn().mockName('mockStatus');
+    const end = jest.fn().mockName('mockEnd');
 
     req = mockRequest({
       method: 'GET',
@@ -32,22 +45,32 @@ describe('/.well-known/openid-configuration', () => {
       status,
       end,
     }) as any;
-
-    jest.isolateModules(() => {
-      configuration = require('~/lib/shared/config/openid-configuration')
-        .default;
-    });
   });
 
-  it('should fetch a the OpenID Configuration', async () => {
+  it('should fetch the OpenID Configuration', async () => {
     await fetchConfiguration(req, res);
 
     expect(res.setHeader).toHaveBeenCalledWith(
       'X-Robots-Tag',
       'noindex, nofollow'
     );
-    expect(res.json).toHaveBeenCalledWith(configuration());
-    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).toHaveBeenCalledWith(mockConfiguration());
     expect(res.end).not.toHaveBeenCalled();
+  });
+  
+  it('should return status 500 upon failure', async () => {
+    process.env = {
+      ...process.env,
+      PROVIDER_URL: undefined,
+    }
+    
+    await fetchConfiguration(req, res);
+
+    expect(res.setHeader).toHaveBeenCalledWith(
+      'X-Robots-Tag',
+      'noindex, nofollow'
+    );
+    expect(res.json).not.toHaveBeenCalled();
+    expect(res.end).toHaveBeenCalledWith('Internal Server Error');
   });
 });
