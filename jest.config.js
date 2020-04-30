e3a8578be9af4901d1e4902bdf98540fba8b96c8
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-04-30 10:21:14 +0200

    test: fixed Jest test setup

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-
 module.exports = {
-  globals: { ...process.env },
   moduleNameMapper: {
     '^~/(.*)': '<rootDir>/$1',
   },
   preset: '@shelf/jest-mongodb',
+  setupFiles: ['<rootDir>/lib/test/setup.js'],
   testEnvironment: 'node',
 };
--- lib/shared/db/connect.test.ts
+++ lib/shared/db/connect.test.ts
@@ -3,10 +3,13 @@ import mongoose, { Mongoose } from 'mongoose';
 import connect from '~/lib/shared/db/connect';
 
 describe('MongoDB Connection', () => {
+  afterEach(() => {
+    mongoose.connection.close();
+  });
+
   it('connects', async () => {
     const connection: Promise<Mongoose> = connect();
 
     await expect(connection).resolves.toBeTruthy();
-    mongoose.connection.close();
   });
 });
