6d729fdb238792a46c340f62cdbc16441235be24
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-06-07 11:50:39 +0200

    chore: updated dependencies, fixed type definition

--- lib/shared/middleware/cors.ts
+++ lib/shared/middleware/cors.ts
@@ -4,12 +4,12 @@ import crossOrigin, { CorsOptions, CorsOptionsDelegate } from 'cors';
 const middleware = async (
   req: NextApiRequest,
   res: NextApiResponse,
-  options: CorsOptions | CorsOptionsDelegate,
+  options: CorsOptions | CorsOptionsDelegate
 ): Promise<boolean> =>
-  new Promise((resolve) => {
+  new Promise(resolve => {
     const cors = crossOrigin(options);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    cors(req as any, res as any, (err: Error) => {
+    cors(req as any, res as any, (err: any) => {
       if (err instanceof Error) {
         res.status(500);
         return resolve(false);
