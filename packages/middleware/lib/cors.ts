f0050f6df0ed670bfce17579f2dd2dedb63d2fb2
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2021-06-05 19:32:53 +0200

    feat(middleware): enhanced CORS middleware

--- packages/middleware/lib/cors.ts
+++ packages/middleware/lib/cors.ts
@@ -6,12 +6,6 @@ import { METHOD } from 'utils/lib/types/method';
 import HTTPError from 'utils/lib/util/http_error';
 
 const MAX_AGE = 86400;
-const ALLOW_HEADERS = [
-  'Accept',
-  'Accept-Language',
-  'Content-Language',
-  'Content-Type',
-];
 const ALLOW_METHODS = [METHOD.GET, METHOD.HEAD];
 
 export type CorsOptions = {
@@ -33,6 +27,7 @@ enum CORS_HEADERS {
   REQUEST_HEADERS = 'access-control-request-headers',
   REQUEST_METHOD = 'access-control-request-method',
   ORIGIN = 'origin',
+  VARY = 'vary',
 }
 
 const middleware = async (
@@ -41,21 +36,29 @@ const middleware = async (
   options: CorsOptions
 ): Promise<boolean> =>
   new Promise((resolve) => {
-    const [fallbackOrigin] = ALLOWED_ORIGINS;
+    const vary: string[] = [];
+
+    const requestHeaders = req.headers[CORS_HEADERS.REQUEST_HEADERS] as string;
+    const requestHeadersArray: string[] = Array.isArray(requestHeaders)
+      ? requestHeaders
+      : requestHeaders?.length > 0
+      ? requestHeaders
+          .split(',')
+          .map((header: string) => header.trim())
+          .filter((header: string) => header.length > 0)
+      : [];
 
     const headers = new Map<CORS_HEADERS, number | string>([
       [CORS_HEADERS.MAX_AGE, options.maxAge ?? MAX_AGE],
-      [
-        CORS_HEADERS.ALLOW_HEADERS,
-        (options.headers || ALLOW_HEADERS).join(', '),
-      ],
       [
         CORS_HEADERS.ALLOW_METHODS,
         (options.methods || ALLOW_METHODS).join(', '),
       ],
     ]);
+
     options.credentials && headers.set(CORS_HEADERS.ALLOW_CREDENTIALS, 'true');
-    options.mirrorOrigin || !fallbackOrigin?.length
+
+    options.mirrorOrigin
       ? headers.set(
           CORS_HEADERS.ALLOW_ORIGIN,
           req.headers[CORS_HEADERS.ORIGIN] as string
@@ -65,7 +68,23 @@ const middleware = async (
           CORS_HEADERS.ALLOW_ORIGIN,
           req.headers[CORS_HEADERS.ORIGIN] as string
         )
-      : headers.set(CORS_HEADERS.ORIGIN, fallbackOrigin);
+      : null;
+
+    headers.get(CORS_HEADERS.ALLOW_ORIGIN) && vary.push(CORS_HEADERS.ORIGIN);
+
+    (options.headers ?? requestHeadersArray).length > 0 &&
+      headers.set(
+        CORS_HEADERS.ALLOW_HEADERS,
+        (options.methods || requestHeadersArray).join(', ')
+      ) &&
+      vary.push(CORS_HEADERS.REQUEST_HEADERS);
+
+    headers.set(CORS_HEADERS.VARY, vary.join(', '));
+
+    headers.get(CORS_HEADERS.ALLOW_ORIGIN) &&
+      headers.forEach((value: number | string, key: CORS_HEADERS) =>
+        res.setHeader(key, value)
+      );
 
     if (req.method !== METHOD.OPTIONS) {
       return resolve(false);
@@ -84,9 +103,8 @@ const middleware = async (
       );
     }
 
-    headers.forEach((value: number | string, key: CORS_HEADERS) =>
-      res.setHeader(key, value)
-    );
+    res.writeHead(STATUS_CODE.NO_CONTENT, { 'Content-Length': 0 });
+
     return resolve(true);
   });
 
